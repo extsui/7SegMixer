df1b7856ea744e39f6998fb8d0543b883576c5d0
extsui <en422641@s.okayama-u.ac.jp>
2018-07-04 23:50:01 +0900

    [update] 全7セグを0-9で繰り返す表示を追加
    LATCHの制御を追加して全更新可能にした。

--- ArmorPanelSPI.py
+++ ArmorPanelSPI.py
@@ -81,37 +81,33 @@ def to_real_panel(panel):
 
 # 7SegArmorの実配列からコマンドへの変換
 def to_armor_command(real_panel):
-	command = ''
-
-	# コマンドはArmor5-->4-->...-->0の順に送信する必要がある
-
-	#for armor in range(5, -1, -1):
+	command = []
 	for armor in range(6):
-		command += '#0'
 		for y in range(4):
-			command += '01'
+			command.append(0x01)
 			for x in range(8):
-				command += ('%02x' % real_panel[armor][y][x])
-		command += '\n'
+				command.append(real_panel[armor][y][x])
 	return command
 
 # パネルからコマンドへの変換
 def panel_to_command(panel):
 	real_panel = to_real_panel(panel)
 	command = to_armor_command(real_panel)
-	command += '#1\n'
 	return command
-
-"""
-for y in range(8):
-	for x in range(24):
-		panel[y][x] = 0xFF
-		print(panel_to_command(panel))
-"""
 
 import spidev
 import time
-import sys
+
+# LATCHピンの指定
+# GPIO26(37番ピン)を使用する。
+import RPi.GPIO as GPIO
+
+# ピン番号ではなく、機能名(例:GPIO26)で指定できるようにする。
+GPIO.setmode(GPIO.BCM)
+
+# 出力設定
+GPIO.setup(26, GPIO.OUT)
+
 
 def reverse_bit_order(x):
 	x_reversed = 0x00
@@ -133,12 +129,90 @@ def reverse_bit_order(x):
 		x_reversed |= 0x80
 	return x_reversed
 	
-
 spi = spidev.SpiDev()
 spi.open(0, 0)
 spi.mode = 0
 spi.max_speed_hz = 1000000	# 1MHz
 
-xfer_data = map(reverse_bit_order, xfer_data)
+start_time = time.time()
+"""
+for y in range(8):
+	for x in range(24):
+		panel[y][x] = 0xFF
+		xfer_data = panel_to_command(panel)
+		xfer_data = map(reverse_bit_order, xfer_data)
+		
+		for i in range(6):
+			spi.writebytes(xfer_data[i*36:(i+1)*36])
+			#time.sleep(0.001)
+			
+		time.sleep(0.006)
+		
+		# 表示更新(LATCH=＿|￣|＿)
+		GPIO.output(26, GPIO.HIGH)
+		GPIO.output(26, GPIO.LOW)
+		
+		print('%d' % (x + y*24))
 
+
+for y in range(8):
+	for x in range(24):
+		panel[y][x] = 0x00
+		xfer_data = panel_to_command(panel)
+		xfer_data = map(reverse_bit_order, xfer_data)
+		
+		for i in range(6):
+			spi.writebytes(xfer_data[i*36:(i+1)*36])
+			#time.sleep(0.001)
+			
+		time.sleep(0.006)
+		
+		# 表示更新(LATCH=＿|￣|＿)
+		GPIO.output(26, GPIO.HIGH)
+		GPIO.output(26, GPIO.LOW)
+		
+		print('%d' % (x + y*24))
+"""
+
+
+num_to_pattern = [
+	0xfc, # 0
+	0x60, # 1
+	0xda, # 2
+	0xf2, # 3
+	0x66, # 4
+	0xb6, # 5
+	0xbe, # 6
+	0xe4, # 7
+	0xfe, # 8
+	0xf6, # 9
+]
+	
+while (True):
+	for ptn in num_to_pattern:
+		
+		panel = [[ptn for x in range(24)] for y in range(8)]
+		
+		xfer_data = panel_to_command(panel)
+		xfer_data = map(reverse_bit_order, xfer_data)
+		
+		for i in range(6):
+			spi.writebytes(xfer_data[i*36:(i+1)*36])
+			#time.sleep(0.010)
+			
+		time.sleep(0.010)
+		
+		# 表示更新(LATCH=＿|￣|＿)
+		GPIO.output(26, GPIO.HIGH)
+		GPIO.output(26, GPIO.LOW)
+		
+		time.sleep(0.0100)
+		
+elapsed_time = time.time() - start_time
+
+print('%f [s]' % elapsed_time)
+print('fps = %f' % (24*8. / elapsed_time))
+
+GPIO.cleanup()
+print('Done.')
 
